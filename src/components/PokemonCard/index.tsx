708f11fe807a2fda07d4743b3acb0ffd05d17adc
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-07-21 20:24:11 -0300

    Style PokemonCard component

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 function PokemonCard() {
-  return <div>PokemonCard</div>;
+  return (
+    <div className={styles.card}>
+      <img src="/" alt="Insert image here" />
+      <h1>Pikachu</h1>
+    </div>
+  );
 }
 
 export default PokemonCard;
