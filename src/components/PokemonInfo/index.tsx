cf07d75b84b18d11e336ff465ca4661561e0fe07
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-07-24 14:50:28 -0300

    Style info modal

--- src/components/PokemonInfo/index.tsx
+++ src/components/PokemonInfo/index.tsx
@@ -17,7 +17,25 @@ interface Pokemon {
 }
 
 function PokemonInfo({ pokemon }: PokemonInfoProps) {
-  return <div className={styles.container}>Pokemon Info</div>;
+  console.log(`pokemon`, pokemon);
+
+  return (
+    <main className={styles.modal}>
+      <div className={styles.modalContainer}>
+        <div className={styles.modalColumn}>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <h2>Description</h2>
+        </div>
+        <div className={styles.modalColumn}>
+          <h1>Name</h1>
+          <h2>Description</h2>
+          <h2>Abilities</h2>
+          <h2>Speed</h2>
+          <h2>Attack</h2>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default PokemonInfo;
