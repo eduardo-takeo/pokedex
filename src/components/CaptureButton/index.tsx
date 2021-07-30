71f18039e3d2a9c79afe069ce266795043a06d91
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-07-30 20:13:47 -0300

    Hide Capture button when info modal is open

--- src/components/CaptureButton/index.tsx
+++ src/components/CaptureButton/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./styles.module.scss";
 import PokeballIcon from "../../assets/pokeball.png";
+import { useModal } from "../../contexts/ModalContext";
 
 interface CaptureButtonProps {
   onClick: () => void;
 }
 
 function CaptureButton({ onClick }: CaptureButtonProps) {
+  const { isVisible } = useModal();
+
   return (
-    <button onClick={onClick}>
-      <img src={PokeballIcon} alt="Capture" />
-    </button>
+    <>
+      {!isVisible && (
+        <button onClick={onClick}>
+          <img src={PokeballIcon} alt="Capture" />
+        </button>
+      )}
+    </>
   );
 }
 
