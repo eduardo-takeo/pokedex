e29f04bfee9691f6ffdae807481d93ec2e2a4d6a
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-08-02 19:32:06 -0300

    Test if button is hiding when modal is open

--- src/components/CaptureButton/CaptureButton.spec.tsx
+++ src/components/CaptureButton/CaptureButton.spec.tsx
@@ -1,6 +1,28 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CaptureButton from ".";
+import PokemonCard from "../PokemonCard";
+import { ModalContextProvider } from "../../contexts/ModalContext";
+
+const mockedPokemon = {
+  height: 2,
+  id: 50,
+  name: "diglett",
+  sprites: {
+    front_default:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/50.png",
+    front_shiny:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/50.png",
+  },
+  types: [
+    {
+      type: {
+        name: "ground",
+      },
+    },
+  ],
+  weight: 8,
+};
 
 describe("CaptureButton component", () => {
   it("should render correctly", () => {
@@ -20,5 +42,18 @@ describe("CaptureButton component", () => {
     expect(captureButton).toContainElement(
       screen.getByRole("img", { name: "Capture" })
     );
+  });
+
+  it("should hide when PokemonInfo modal is open", () => {
+    //? toggleModal (declared inside context) was not being found, wrap component inside the ContextProvider to fix
+    render(
+      <ModalContextProvider>
+        <PokemonCard pokemon={mockedPokemon} />
+      </ModalContextProvider>
+    );
+    fireEvent.click(screen.getByText("diglett", { exact: false }));
+
+    const captureButton = screen.queryByRole("img", { name: "Capture" });
+    expect(captureButton).not.toBeInTheDocument();
   });
 });
